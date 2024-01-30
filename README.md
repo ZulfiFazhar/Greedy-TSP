3995c9aa586b4a9be4639618ff7af430f286d6ae
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2024-01-30 14:53:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Traveling Salesman Problem (TSP) Dashboard
+
+Aplikasi ini dibuat untuk menyelesaikan masalah Traveling Salesman Problem (TSP) menggunakan algoritma greedy. Aplikasi ini dibangun dengan Streamlit dan menggunakan library geopy untuk menghitung jarak antara kota-kota dan Folium untuk memvisualisasikan rute TSP pada peta.
+
+[link text](https:)
+
+## Cara Menggunakan
+
+1. Masukkan jumlah kota yang ingin Anda kunjungi.
+2. Untuk setiap kota, masukkan nama kota dan koordinatnya (latitude dan longitude) dalam format 'lat, lon'.
+3. Pilih kota awal dari daftar kota yang telah Anda masukkan.
+4. Aplikasi akan menghitung rute TSP dan menampilkan urutan kota yang harus dikunjungi.
+5. Aplikasi juga akan menampilkan peta dengan rute TSP yang dihasilkan.
+
+## Fungsi Utama
+
+- `input_kota_coords()`: Fungsi ini digunakan untuk meminta pengguna memasukkan jumlah kota, nama kota, dan koordinat kota. Fungsi ini mengembalikan kamus di mana kunci adalah nama kota dan nilai adalah tuple koordinat kota.
+- `hitung_jarak(kota1, kota2, kota_coords)`: Fungsi ini menghitung jarak antara dua kota menggunakan koordinat mereka.
+- `tsp_greedy(start_kota, kota_coords)`: Fungsi ini mengimplementasikan algoritma greedy untuk menyelesaikan TSP. Fungsi ini mengembalikan urutan kota yang harus dikunjungi untuk meminimalkan jarak total.
+
+## Catatan
+
+- Pastikan Anda memasukkan koordinat dengan benar dan dalam format yang benar ('lat, lon').
+- Aplikasi ini menggunakan algoritma greedy, yang mungkin tidak selalu menghasilkan solusi optimal untuk TSP.
---
+++ index.py
@@ -0,0 +1,93 @@
+import streamlit as st
+from geopy.distance import geodesic
+import folium
+
+# Membuat funtion untuk nantinya dipakai
+
+def input_kota_coords():
+    kota_coords = {}
+    jumlah_kota = st.number_input("Masukkan jumlah kota:", min_value=2, step=1)
+
+    for i in range(jumlah_kota):
+        nama_kota = st.text_input(f"Masukkan nama kota ke-{i+1}:", key=f"Kota_{i}")
+        try :
+            lat_lon = st.text_input(f"Masukkan latitude dan longitude untuk kota {nama_kota} (pisahkan dengan koma):", key=f"LatLon_{i}")
+            lat, lon = map(float, lat_lon.split(','))
+            kota_coords[nama_kota] = (lat, lon)
+        except ValueError :
+            st.error("Mohon masukkan Latitude dan Longitude dalam format")
+
+    return kota_coords
+
+def hitung_jarak(kota1, kota2, kota_coords):
+    return geodesic(kota_coords[kota1], kota_coords[kota2]).kilometers
+
+def tsp_greedy(start_kota, kota_coords):
+    rute = [start_kota]
+    kota_belum_dikunjungi = list(kota_coords.keys())
+    kota_belum_dikunjungi.remove(start_kota)
+
+    while kota_belum_dikunjungi:
+        kota_saat_ini = rute[-1]
+        jarak_minimal = float('inf')
+        kota_terdekat = None
+
+        for kota in kota_belum_dikunjungi:
+            jarak = hitung_jarak(kota_saat_ini, kota, kota_coords)
+            if jarak < jarak_minimal:
+                jarak_minimal = jarak
+                kota_terdekat = kota
+
+        rute.append(kota_terdekat)
+        kota_belum_dikunjungi.remove(kota_terdekat)
+
+    return rute
+
+# Streamlit UI
+st.title("Traveling Salesman Problem (TSP) Dashboard")
+
+# Input kota_coords dari pengguna
+kota_coords = input_kota_coords()
+
+# Input start_kota dari pengguna
+start_kota = st.selectbox("Pilih nama kota start:", list(kota_coords.keys()))
+
+# Hitung rute TSP
+try :
+    rute_tsp = tsp_greedy(start_kota, kota_coords)
+except ValueError :
+    st.error("No Value")
+
+# Tampilkan hasil rute
+st.subheader("Solusi TSP")
+try :
+    rute_string = ' - '.join(rute_tsp)
+    st.write(f"{rute_string} - {start_kota}")
+except NameError :
+    st.error("No Value")
+
+# Buat peta TSP
+try :
+    peta_tsp = folium.Map(location=kota_coords[start_kota], zoom_start=13)
+except KeyError :
+    print("")
+
+for kota in kota_coords:
+    folium.Marker(kota_coords[kota], popup=kota).add_to(peta_tsp)
+
+try :
+    koordinat_rute = [kota_coords[k] for k in rute_tsp] + [kota_coords[start_kota]]
+    folium.PolyLine(koordinat_rute, color='red', weight=2.5, opacity=1).add_to(peta_tsp)
+    legenda = f"Rute TSP dari {start_kota}: {' - '.join(rute_tsp)} - {start_kota}"
+    folium.Marker(location=kota_coords[start_kota], popup=legenda, icon=folium.Icon(color='green')).add_to(peta_tsp)
+except NameError :
+    print("")
+
+# Tampilkan peta di Streamlit
+st.subheader("Peta Rute TSP")
+
+try :    
+    html_map = peta_tsp._repr_html_()
+    st.components.v1.html(html_map, width=800, height=800)
+except NameError :
+    st.error("No Value")
