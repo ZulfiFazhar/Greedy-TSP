13048c6b84803e2a8b14fa202dcefb653b821827
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2024-02-09 15:25:13 +0700

    Add new Location

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import folium
 
 # Membuat function
 
-def kota():
+def kota_bandung():
     kota_coords = {
         "Bandung Kulon": (-6.9174639, 107.6191228),
         "Babakan Ciparay": (-6.942333, 107.5771444),
@@ -36,6 +36,39 @@ def kota():
         "Sukajadi": (-6.8903936, 107.5889804),
         "Sukasari": (-6.866465412066975, 107.5841221316536),
         "Cidadap": (-6.863482, 107.5904569)
+    }
+
+    return kota_coords
+
+def jawa_barat():
+    kota_coords = {
+        "Kota Sukabumi": (-6.9369269, 106.8761097),
+        "Kota Bogor": (-6.5950162, 106.7094886),
+        "Kota Bandung": (-6.903272, 107.5607541),
+        "Kota Cirebon": (-6.7426699, 108.4063758),
+        "Kota Bekasi": (-6.2841152, 106.6779812),
+        "Kota Depok": (-6.3876714, 106.7353941),
+        "Kota Cimahi": (-6.8861666, 107.4672986),
+        "Kota Tasikmalaya": (-7.3598042, 108.1503496),
+        "Kota Banjar": (-7.3723955, 108.4950041),
+        "Kabupaten Sukabumi": (-6.8492295, 106.8729016),
+        "Kabupaten Cianjur": (-7.0515528, 106.4708709),
+        "Kabupaten Bogor": (-6.5445738, 106.4844121),
+        "Kabupaten Bandung": (-7.0626751, 107.2614852),
+        "Kabupaten Garut": (-7.3414811, 107.1185812),
+        "Kabupaten Tasikmalaya": (-7.4258211, 107.5138864),
+        "Kabupaten Ciamis": (-7.3105189, 108.1177301),
+        "Kabupaten Kuningan": (-6.9870978, 108.2593337),
+        "Kabupaten Cirebon": (-6.760507, 108.2556177),
+        "Kabupaten Majalengka": (-6.8061461, 107.6339381),
+        "Kabupaten Sumedang": (-6.809099, 107.6508147),
+        "Kabupaten Indramayu": (-6.4417654, 107.8669897),
+        "Kabupaten Subang": (-6.809099, 107.6508147),
+        "Kabupaten Purwakarta": (-6.5920294, 107.2451147),
+        "Kabupaten Karawang": (-6.2640209, 107.0340434),
+        "Kabupaten Bekasi": (-6.2669317, 106.7772559),
+        "Kabupaten Bandung Barat": (-6.904469, 107.1245476),
+        "Kabupaten Pangandaran": (-7.6396528, 108.2270064)
     }
 
     return kota_coords
@@ -79,6 +112,53 @@ def tsp_greedy(start_kota, kota_coords):
 
     return rute
 
+def tsp(coords):
+        start_kota = st.selectbox("Pilih Nama Lokasi Keberangkatan:", list(coords.keys()))
+
+        try :
+            rute_tsp = tsp_greedy(start_kota, coords)
+        except ValueError :
+            st.error("No Value")
+
+        # Tampilkan hasil rute
+        st.subheader("Solusi TSP")
+        try :
+            rute_string = ' - '.join(rute_tsp)
+            st.write(f"{rute_string} - {start_kota}")
+        except NameError :
+            st.error("No Value")
+
+        # Buat peta TSP
+        try :
+            peta_tsp = folium.Map(location=coords[start_kota], zoom_start=12)
+        except KeyError :
+            print("")
+
+        for kota in coords:
+            folium.Marker(coords[kota], popup=kota).add_to(peta_tsp)
+
+        try :
+            koordinat_rute = [coords[k] for k in rute_tsp] + [coords[start_kota]]
+
+            folium.PolyLine(koordinat_rute, color='red', weight=2.5, opacity=1).add_to(peta_tsp)
+            
+            folium.Marker(
+                location=coords[start_kota], 
+                popup=start_kota,
+                icon=folium.Icon(color='green')
+                ).add_to(peta_tsp)
+        except NameError :
+            print("")
+
+        # Tampilkan peta di Streamlit
+        st.subheader("Peta Rute TSP")
+
+        try :    
+            html_map = peta_tsp._repr_html_()
+            st.components.v1.html(html_map, width=800, height=490)
+        except NameError :
+            st.error("No Value")
+
 # Streamlit UI
 st.title("Travelling Salesman Problem (TSP) Dashboard")
 
@@ -87,92 +167,17 @@ tab1, tab2 = st.tabs(['Pilih Kota', 'Input Kota'])
 with tab1:
     st.write('Pilih Kota')
 
-    kota_coords = kota()
+    bandung = kota_bandung()
+    kota = jawa_barat()
 
-    start_kota = st.selectbox("Pilih nama kota start:", list(kota_coords.keys()))
+    pilihan = st.selectbox("Pilih Lokasi", ("Kota Bandung", "Jawa Barat"))
 
-    try :
-        rute_tsp = tsp_greedy(start_kota, kota_coords)
-    except ValueError :
-        st.error("No Value")
-
-    # Tampilkan hasil rute
-    st.subheader("Solusi TSP")
-    try :
-        rute_string = ' - '.join(rute_tsp)
-        st.write(f"{rute_string} - {start_kota}")
-    except NameError :
-        st.error("No Value")
-
-    # Buat peta TSP
-    try :
-        peta_tsp = folium.Map(location=kota_coords[start_kota], zoom_start=12)
-    except KeyError :
-        print("")
-
-    for kota in kota_coords:
-        folium.Marker(kota_coords[kota], popup=kota).add_to(peta_tsp)
-
-    try :
-        koordinat_rute = [kota_coords[k] for k in rute_tsp] + [kota_coords[start_kota]]
-        folium.PolyLine(koordinat_rute, color='red', weight=2.5, opacity=1).add_to(peta_tsp)
-        legenda = f"Rute TSP dari {start_kota}: {' - '.join(rute_tsp)} - {start_kota}"
-        folium.Marker(location=kota_coords[start_kota], popup=start_kota, icon=folium.Icon(color='green')).add_to(peta_tsp)
-    except NameError :
-        print("")
-
-    # Tampilkan peta di Streamlit
-    st.subheader("Peta Rute TSP")
-
-    try :    
-        html_map = peta_tsp._repr_html_()
-        st.components.v1.html(html_map, width=800, height=490)
-    except NameError :
-        st.error("No Value")
+    if pilihan == "Kota Bandung":
+        tsp(bandung)
+    elif pilihan == "Jawa Barat":
+        tsp(kota)
 
 with tab2:
     # Input kota_coords
     kota_coords2 = input_kota_coords()
-
-    # Input start_kota
-    start_kota2 = st.selectbox("Pilih nama kota start:", list(kota_coords2.keys()))
-
-    # Hitung rute TSP
-    try :
-        rute_tsp2 = tsp_greedy(start_kota2, kota_coords2)
-    except ValueError :
-        st.error("No Value")
-
-    # Tampilkan hasil rute
-    st.subheader("Solusi TSP")
-    try :
-        rute_string2 = ' - '.join(rute_tsp2)
-        st.write(f"{rute_string2} - {start_kota2}")
-    except NameError :
-        st.error("No Value")
-
-    # Buat peta TSP
-    try :
-        peta_tsp2 = folium.Map(location=kota_coords2[start_kota2], zoom_start=13)
-    except KeyError :
-        print("")
-
-    for kota in kota_coords2:
-        folium.Marker(kota_coords2[kota], popup=kota).add_to(peta_tsp2)
-
-    try :
-        koordinat_rute2 = [kota_coords2[k] for k in rute_tsp2] + [kota_coords2[start_kota2]]
-        folium.PolyLine(koordinat_rute2, color='red', weight=2.5, opacity=1).add_to(peta_tsp2)
-        legenda2 = f"Rute TSP dari {start_kota2}: {' - '.join(rute_tsp2)} - {start_kota2}"
-        folium.Marker(location=kota_coords2[start_kota2], popup=legenda2, icon=folium.Icon(color='green')).add_to(peta_tsp2)
-    except NameError :
-        print("")
-
-    # Tampilkan peta di Streamlit
-    st.subheader("Peta Rute TSP")
-
-    try :    
-        html_map2 = peta_tsp2._repr_html_()
-        st.components.v1.html(html_map2, width=800, height=490)
-    except NameError :
-        st.error("No Value")
+    tsp(kota_coords2)
